64099d8a8e82c030ce7636a17fafddf99d5e6719
Rama0510 <111701613+Rama0510@users.noreply.github.com>
2022-10-25 16:42:39 -0500

    new endpoint

---
+++ routes/compuatition.js
@@ -0,0 +1,30 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET Computation page. */
+router.get('/', function(req,res,next) {
+    var num1 = Math.floor(Math.random() * 10);
+    var num2 = Math.floor(Math.random() * 10);
+    var num3 = Math.floor(Math.random() * 10);
+    var num4 = Math.floor(Math.random() * 10);
+    var atan2Function = Math.atan2(num1,num2);
+    var atanhFunction = Math.atanh(num3);
+    var cbrtFunction = Math.cbrt(num4);
+
+    res.render('computation',
+    {
+        title: 'Ramadevi Redrouthu computing functions',
+        num1: num1,
+        num2: num2,
+        num3: num3,
+        num4: num4,
+        atan2: atan2Function,
+        atanh: atanhFunction,
+        cbrt: cbrtFunction,
+        
+     });
+
+    });
+
+
+    module.exports = router;

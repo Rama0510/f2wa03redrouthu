4276ace14a9675c0fe6e0d4ff215e6de6b4ac0c2
Rama0510 <111701613+Rama0510@users.noreply.github.com>
2022-10-25 21:36:36 -0500

    added bonus

--- routes/computation.js
+++ routes/computation.js
@@ -12,7 +12,7 @@ router.get('/', function(req,res,next) {
 
     var num2 = Math.floor(Math.random() * 10);
 
-    var num3 = Math.floor(Math.random() * 0.9);
+    let num3 = Math.floor(Math.random() * 0.9);
 
     var num4 = Math.floor(Math.random() * 10);
 
